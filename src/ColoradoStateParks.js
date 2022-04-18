870212bc59786e8579ef6fbb686cbcd8c928b991
Saung Oo Myat <saung.myat@gmail.com>
2022-04-17 21:11:03 -0400

    commit tree 36b15a971b0a502abf0145d53e2a9bc0c213ee8f

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,5 +1,15 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
+
+export default function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
+}
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
